bcc587b4c1ba2f9fb0db5cc72288e52a867a4b86
timbotimber <hitimstephens@gmail.com>
2020-06-17 11:18:37 +0200

    fixed bg

--- game.js
+++ game.js
@@ -1,30 +1,26 @@
-console.log('game.js file is linked');
-
+console.log("game.js file is linked");
 
 class Game {
-    constructor() {
-        this.background = new Background();
-        this.player = new Player();
-        this.obstacles = [];
-    }
+  constructor() {
+    this.background = new Background();
+    this.player = new Player();
+    this.obstacles = [];
+  }
 
-    preloadBackgoundImages() {
-        this.backgroundImgs = [
-            { src: loadImage("assets/background/1.jpg"), x: 0, speed: 0 },
+  preloadBackgoundImages() {
+    this.backgroundImgs = [
+      { src: loadImage("assets/background/1.jpg"), x: 0, speed: 0 },
+    ];
 
-        ];
+    this.unicornImg = loadImage("assets/unicorn/Final/unicorn-right.gif");
+    this.gemImg = loadImage("assets/gems/Final/red.jpg");
+  }
 
-        this.unicornImg = loadImage("assets/unicorn/Final/unicorn-right.gif");
-        this.gemImg = loadImage("assets/gems/Final/red.jpg");
+  draw() {
+    //clear();
+    //frameRate(20);
 
-        
-    }
-
-    drawingGame() {
-        //clear();
-        //frameRate(20);       
-
-        this.background.drawBackground();
-        this.player.drawingTheUnicorn();
-    }
+    this.background.drawBackground();
+    this.player.drawingTheUnicorn();
+  }
 }
