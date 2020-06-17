6b04fd4b502da3265d6288074de174a90522fc2f
timbotimber <hitimstephens@gmail.com>
2020-06-17 17:34:50 +0200

    fixed movement

--- player.js
+++ player.js
@@ -1,57 +1,55 @@
-console.log('player.js file is linked');
+console.log("player.js file is linked");
 class Player {
-    constructor() {
-        this.gravity = 0.1;
-        this.speed = 0;
-        this.jumps = 0;
-        this.x = 200;
-        this.moveRight = 0;
+  constructor() {
+    this.gravity = 0.1;
+    this.speed = 0;
+    this.jumps = 0;
+    this.x = 200;
+    this.moveRight = 0;
+  }
+
+  setupPlayer() {
+    this.y = height - game.unicornImg.height;
+    this.width = game.unicornImg.width;
+    this.height = game.unicornImg.height;
+  }
+
+  jump() {
+    this.jumps += 1;
+    // check how many times the player before touching the ground
+    if (this.jumps < 2) {
+      this.speed = -5;
     }
 
-    setupPlayer() {
-        this.y = height - game.unicornImg.height;
-        this.width = game.unicornImg.width;
-        this.height = game.unicornImg.height;
-    }
-
-    jump() {
-        this.jumps += 1;
-
-        // check how many times the player before touching the ground
-        if (this.jumps < 2) {
-            this.speed = -5;
-        }
-
-
-        // ANOTHER APPROACH WITHOUT THE JUMP VARIABLES
-        /*  if(this.y >= height - game.playerImg.height){
+    // ANOTHER APPROACH WITHOUT THE JUMP VARIABLES
+    /*  if(this.y >= height - game.playerImg.height){
             this.speed = -5;
         } */
+  }
+
+  MoveRight() {
+    this.x += 20;
+  }
+
+  MoveLeft() {
+    this.x -= 20;
+  }
+
+  drawingTheUnicorn() {
+    //gravity pushes poor antonio down in the canvas
+    this.speed += this.gravity;
+    this.y += this.speed;
+
+    // console.log(this.speed.toFixed(2));
+    // this if statement is to check that gravity doesnt affect antonio if he is outside of the canvas :D
+
+    if (this.y >= height - game.unicornImg.height) {
+      this.y = height - game.unicornImg.height;
+      //we set the jumps to 0 so we can jump again
+      this.jumps = 0;
     }
 
-    moveRight() {
-        //this.x -= 10
-        console.log(" D was pressed");
-    }
-
-
-    drawingTheUnicorn() {
-        //gravity pushes poor antonio down in the canvas
-        this.speed += this.gravity;
-        this.y += this.speed;
-
-        // console.log(this.speed.toFixed(2));
-        // this if statement is to check that gravity doesnt affect antonio if he is outside of the canvas :D
-
-        if (this.y >= height - game.unicornImg.height) {
-            this.y = height - game.unicornImg.height;
-            //we set the jumps to 0 so we can jump again
-            this.jumps = 0;
-        }
-
-        // here i dont pass any width or height into the image function so the image takes the original size of the running man
-        image(game.unicornImg, this.x, this.y);
-
-    }
-
+    // here i dont pass any width or height into the image function so the image takes the original size of the running man
+    image(game.unicornImg, this.x, this.y);
+  }
 }
